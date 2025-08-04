bb621587087517fa4dfd3dd4e9875f923a928bab
Anshika Kumari <ak0379@srmist.edu.in>
2025-08-04 17:21:43 +0530

    Bit Manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 public class BitManipulation{
     public static void main(String args[]){
         //Get Bit
+        Scanner sc= new Scanner (System.in);
         int n=5;
         int pos=2;
         int BitMask=1<<pos;
@@ -12,6 +14,45 @@ public class BitManipulation{
         }
 
         //Set Bit
+        int m=5;
+        int position=1;
+        int Bitmask=1<<position;
+        int  newNum=Bitmask | m;
+        System.out.println(newNum);
+
+        //Clear Bit
+        int p=5;
+        int post=2;
+        int bitmask=1<<post;
+        int notBitmask=~(bitmask);
+        int newNumber=notBitmask&p;
+        System.out.println(newNumber);
+
+        //Update Bit
+        int oper=sc.nextInt();   //update bit o 1 else pdate bit to 0
+        //oper=1 : set, oper=0 : clear
+        int q=5;   //0101
+        int pOs=1;
+
+        //set bit
+        if(oper==1){
+            int bitMask=1<<pOs;
+            int newnum=bitMask | q;
+            System.out.println(newnum);
+        }
+
+        else{
+            int BiTmask=1<<pOs;
+            int NotBitmask=~(BiTmask);
+            int newnumber=notBitmask&q;
+            System.out.println(newnumber);
+
+        }
         
+        
+
+
+
+
     }
 }
